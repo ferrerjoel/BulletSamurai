f214fcdf4529ecaf03b14cd8a56dc3e00b30f1b8
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-11-21 19:04:36 +0100

    Added enemy spiders and AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,80 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
+	public UnityEngine.AI.NavMeshAgent agent;
+
+	public Transform player;
+
+	public LayerMask whatIsGround, whatIsPlayer;
+
+	// Patroling
+
+	public Vector3 walkPoint;
+	bool walkPointSet;
+	public float walkPointRange;
+
+	// Attacking
+	public float timeBetweenAttacks;
+	bool isAtacking;
+
+	//States
+	public float sightRange, attackRange;
+	public bool playerIsInSight, playerIsInAttackRange;
+
+	private void Start()
+	{
+		player = GameObject.Find("jugador").transform;
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		playerIsInSight = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+		playerIsInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+		if (!playerIsInSight && !playerIsInAttackRange) Patroling();
+		if (playerIsInSight && !playerIsInAttackRange) ChasePlayer();
+		if (playerIsInAttackRange && playerIsInSight) ChasePlayer();
+
+	}
+
+
+
+	private void Patroling()
+	{
+		if (!walkPointSet) SearchWalkPoint();
+
+		if (walkPointSet) agent.SetDestination(walkPoint);
+
+		Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+		// Walkpoint reached
+
+		if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
+	}
+
+	private void SearchWalkPoint()
+	{
+		float randomZ = Random.Range(-walkPointRange, walkPointRange);
+		float randomX = Random.Range(-walkPointRange, walkPointRange);
+		walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+		if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
+	}
+
+	private void ChasePlayer()
+	{
+		agent.SetDestination(player.position);
+	}
+
+	private void AttackPlayer()
+	{
+
+	}
+
+	// LEGACY CODE
 	public void takeHit()
 	{
 		Destroy(gameObject);
