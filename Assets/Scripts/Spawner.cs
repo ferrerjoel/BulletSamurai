eadea0f97224a7ded0a08c3a3543dac31a696cf5
Pol Renalias <prenaliasp@inscastellet.cat>
2022-12-05 18:42:15 +0100

    umu

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField]
+	public GameObject spider;
+	
+    void Start()
+    {
+        
+    }
+   
+ 
+    void Update()
+    {
+        if (transform.childCount <= 1)
+		{
+			var instance = Instantiate(spider, gameObject.transform.position, Quaternion.identity);
+			instance.transform.parent = gameObject.transform;
+		}
+    }
+}
